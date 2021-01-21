48942acf5b434c8bbdf899e210b9bcef99ba99d2
kyobon <70905915+kyobon@users.noreply.github.com>
2021-01-21 13:47:30 +0900

    Update settings.py

--- django_onsen/settings.py
+++ django_onsen/settings.py
@@ -23,7 +23,7 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 SECRET_KEY = 'z@u1xzyb^df3=6tecirkub=7=$jtm+u(e+rst8y+645)-o*jl2'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
+DEBUG = True
 
 ALLOWED_HOSTS = ['*']
 
